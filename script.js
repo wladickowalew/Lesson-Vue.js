2e7ed7e7d6e9b75c519f770b1153682c49780183
Wlad Kowalew <wladickowalew@gmail.com>
2019-08-16 11:27:03 +0300

    Ve.js begin

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-let f = document.getElementById("f");
-let t = f.value;
-let a = ["hi", "22", "WEGWRG"];
-generateList(a);
-
-f.addEventListener("input", function(event){
-	t = event.target.value; 
-	console.log(t);
-});
-
-function generateList(array){
-	let list = $("#list");
-	list.html();
-	array.forEach(function(item){
-		list.append("<li>"+item+"</li>");
-	});
-}
+let vm= new Vue({
+	el: "#app",
+	data:{
+		message: "Hello, Vue.js!"
+	}
+})
